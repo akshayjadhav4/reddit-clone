2408ac73b12ad5ecdcad2be514520e8869cfa6aa
AKSHAY <akshayjadhav461999@gmail.com>
2021-02-12 17:21:41 +0530

    User Modal

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,16 +1,54 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Index,
+  CreateDateColumn,
+  BeforeInsert,
+} from "typeorm";
+import { IsEmail, Length } from "class-validator";
+import bcrypt from "bcrypt";
+import { classToPlain, Exclude } from "class-transformer";
 
 @Entity("users")
-export class User {
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+
+  @Exclude()
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  firstName: string;
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
 
-  @Column()
-  lastName: string;
+  @Index()
+  @Length(3)
+  @Column({ unique: true })
+  username: string;
 
+  @Exclude()
+  @Length(6)
   @Column()
-  age: number;
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @CreateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
 }
