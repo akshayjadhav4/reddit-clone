04e4331ddaf1be248754d83214c7e18272b699b3
AKSHAY <akshayjadhav461999@gmail.com>
2021-02-12 17:21:15 +0530

    server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,20 @@ import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
 
+import authRoutes from "./routes/auth";
+import trim from "./middlewares/trim";
+
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(trim);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(2004, async () => {
   console.log("Server Started.");
