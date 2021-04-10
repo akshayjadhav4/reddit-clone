398c057031996ec260a7a59280bc5fb444643e0d
AKSHAY <akshayjadhav461999@gmail.com>
2021-04-10 16:52:33 +0530

    fetching posts

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -25,7 +25,35 @@ const createPost = async (req: Request, res: Response) => {
   }
 };
 
+const getPosts = async (_: Request, res: Response) => {
+  try {
+    const posts = await Post.find({
+      order: { createdAt: "DESC" },
+    });
+    return res.json(posts);
+  } catch (error) {
+    console.log("GET POSTS ERROR");
+    return res.status(500).json({ error: "Something went wrong..." });
+  }
+};
+
+const getPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  try {
+    const post = await Post.findOneOrFail(
+      { identifier, slug },
+      { relations: ["sub"] }
+    );
+    return res.json(post);
+  } catch (error) {
+    console.log("GET POST ERROR");
+    return res.status(404).json({ error: "Post not found." });
+  }
+};
+
 const router = Router();
 router.post("/create", auth, createPost);
+router.get("/getPosts", getPosts);
+router.get("/getPost/:identifier/:slug", getPost);
 
 export default router;
