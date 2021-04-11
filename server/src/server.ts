050229016c51212d67235f537b7ac9a1f90d5196
AKSHAY <akshayjadhav461999@gmail.com>
2021-04-11 17:47:04 +0530

    cookies options

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(trim);
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
