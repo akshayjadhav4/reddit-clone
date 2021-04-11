2e8b6c68b922d1ddd482a7dba0245dde2f429598
AKSHAY <akshayjadhav461999@gmail.com>
2021-04-11 17:26:56 +0530

    register user done

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,7 +1,34 @@
+import { FormEvent, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+import Axios from "axios";
+
+import InputGroup from "../components/InputGroup/InputGroup";
 
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({});
+  const router = useRouter();
+
+  const registerUser = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      await Axios.post("auth/register", {
+        email,
+        password,
+        username,
+      });
+      router.push("/login");
+    } catch (error) {
+      console.log("ERROR WHILE SIGNUP OPERATION", error.response.data);
+      setErrors(error.response.data);
+    }
+  };
+
   return (
     <div className="flex register">
       <Head>
@@ -22,28 +49,31 @@ export default function Register() {
             <span className="text-blue-500 cursor-pointer">Privacy Policy</span>
             .
           </p>
-          <form>
-            <div className="mb-2">
-              <input
-                type="email"
-                className="w-full px-3 py-2 bg-gray-100 border-gray-400 rounded focus:outline-none"
-                placeholder="Email"
-              />
-            </div>
-            <div className="mb-2">
-              <input
-                type="text"
-                className="w-full px-3 py-2 bg-gray-100 border-gray-400 rounded focus:outline-none"
-                placeholder="Username"
-              />
-            </div>
-            <div className="mb-2">
-              <input
-                type="password"
-                className="w-full px-3 py-2 bg-gray-100 border-gray-400 rounded focus:outline-none"
-                placeholder="Confirm Password"
-              />
-            </div>
+          <form onSubmit={registerUser}>
+            <InputGroup
+              className="mb-2"
+              type="email"
+              placeholder="Email"
+              value={email}
+              error={errors.email}
+              setValue={setEmail}
+            />
+            <InputGroup
+              className="mb-2"
+              type="text"
+              placeholder="Username"
+              value={username}
+              error={errors.username}
+              setValue={setUsername}
+            />
+            <InputGroup
+              className="mb-2"
+              type="password"
+              placeholder="Password"
+              value={password}
+              error={errors.password}
+              setValue={setPassword}
+            />
             <button className="w-full py-2 mb-4 text-xs font-bold text-white uppercase bg-blue-500 border-blue-500 rounded hover:bg-blue-400">
               Sign Up
             </button>
